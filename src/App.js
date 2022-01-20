5792f920587d352a5abecc721514e7bb4fc99bed
Alexander Paule <alec.paule@yahoo.com>
2022-01-19 16:39:34 -0800

    Adds numeric and operator buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.scss";
 
 function App() {
+  const [input, setInput] = useState("");
+
   return (
     <div className="App">
-      <header>Calculator</header>
+      <section>
+        <h1>{input}</h1>
+        <div className="buttons">
+          <button onClick={() => setInput("")}>c</button>
+          <button onClick={() => setInput(input + "±")}>±</button>
+          <button onClick={() => setInput(input + "%")}>%</button>
+          <button onClick={() => setInput(input + "/")}>/</button>
+          <button onClick={() => setInput(input + "7")}>7</button>
+          <button onClick={() => setInput(input + "8")}>8</button>
+          <button onClick={() => setInput(input + "9")}>9</button>
+          <button onClick={() => setInput(input + "*")}>*</button>
+          <button onClick={() => setInput(input + "4")}>4</button>
+          <button onClick={() => setInput(input + "5")}>5</button>
+          <button onClick={() => setInput(input + "6")}>6</button>
+          <button onClick={() => setInput(input + "-")}>-</button>
+          <button onClick={() => setInput(input + "1")}>1</button>
+          <button onClick={() => setInput(input + "2")}>2</button>
+          <button onClick={() => setInput(input + "3")}>3</button>
+          <button onClick={() => setInput(input + "+")}>+</button>
+          <button onClick={() => setInput(input + "0")}>0</button>
+          <button onClick={() => setInput(input + ".")}>.</button>
+          <button onClick={() => setInput(input + "=")}>=</button>
+        </div>
+      </section>
     </div>
   );
 }
