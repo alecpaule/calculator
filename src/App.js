eb1ac7d634a91e007816aca56066113d056dbe85
Alexander Paule <alec.paule@yahoo.com>
2022-01-26 17:08:07 -0800

    Changes font size

--- src/App.js
+++ src/App.js
@@ -8,52 +8,34 @@ function App() {
     <div className="App">
       <section>
         <h1>{input}</h1>
-        <button className="specialCharacter" onClick={() => setInput("")}>
+        <button className="operator" onClick={() => setInput("")}>
           c
         </button>
-        <button
-          className="specialCharacter"
-          onClick={() => setInput(input + "±")}
-        >
+        <button className="operator" onClick={() => setInput(input + "±")}>
           ±
         </button>
-        <button
-          className="specialCharacter"
-          onClick={() => setInput(input + "%")}
-        >
+        <button className="operator" onClick={() => setInput(input + "%")}>
           %
         </button>
-        <button
-          className="specialCharacter operator"
-          onClick={() => setInput(input + "/")}
-        >
+        <button className="operator" onClick={() => setInput(input + "/")}>
           /
         </button>
         <button onClick={() => setInput(input + "7")}>7</button>
         <button onClick={() => setInput(input + "8")}>8</button>
         <button onClick={() => setInput(input + "9")}>9</button>
-        <button
-          className="specialCharacter operator"
-          onClick={() => setInput(input + "*")}
-        >
+        <button className="operator" onClick={() => setInput(input + "*")}>
           *
         </button>
         <button onClick={() => setInput(input + "4")}>4</button>
         <button onClick={() => setInput(input + "5")}>5</button>
         <button onClick={() => setInput(input + "6")}>6</button>
-        <button
-          className="specialCharacter operator"
-          onClick={() => setInput(input + "-")}
-        >
+        <button className="operator" onClick={() => setInput(input + "-")}>
           -
         </button>
         <button onClick={() => setInput(input + "1")}>1</button>
         <button onClick={() => setInput(input + "2")}>2</button>
         <button onClick={() => setInput(input + "3")}>3</button>
-        <button
-          className="specialCharacter operator"
-          onClick={() => setInput(input + "+")}
-        >
+        <button className="operator" onClick={() => setInput(input + "+")}>
           +
         </button>
         <button className="zeroButton" onClick={() => setInput(input + "0")}>
