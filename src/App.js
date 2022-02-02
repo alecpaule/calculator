de9832642e4c3d8d81aa4e6c413216db8e700948
Alexander Paule <alec.paule@yahoo.com>
2022-02-02 13:12:32 -0800

    Calculates simple math expressions

--- src/App.js
+++ src/App.js
@@ -1,70 +1,116 @@
 import React, { useState } from "react";
 import "./App.scss";
+import { create, all } from "mathjs";
 
 function App() {
-  const [input, setInput] = useState("");
+  const [display, setDisplay] = useState("");
+  const [operand, setOperand] = useState("");
+  const [firstOperand, setFirstOperand] = useState("");
+  const [operator, setOperator] = useState("");
 
-  const addToInput = (character) => {
-    if (input.length < 10) setInput(input + character);
+  const addToOperand = (digit) => {
+    if (operand.length < 10) {
+      setOperand(operand + digit);
+      setDisplay(operand + digit);
+    }
   };
+
+  const handleSetOperands = () => {
+    if (!firstOperand) {
+      // if firstOperand is empty (i.e. the first calculation)
+      setFirstOperand(operand);
+      setOperand("");
+    } else {
+      // if firstOperand is not empty (i.e. continued calculations, result is set as firstOperand)
+      setOperand("");
+    }
+  };
+
+  const config = {};
+  const math = create(all, config);
 
   return (
     <div className="App">
       <section>
-        <h1>{input}</h1>
-        <button className="operatorButton" onClick={() => setInput("")}>
+        <h1>{display}</h1>
+        <button
+          className="operatorButton"
+          onClick={() => {
+            setDisplay("");
+            setOperand("");
+            setFirstOperand("");
+          }}
+        >
           c
         </button>
-        <button
-          className="operatorButton"
-          onClick={() => setInput(input + "±")}
-        >
+        <button className="operatorButton" onClick={() => console.log("±")}>
           ±
         </button>
-        <button
-          className="operatorButton"
-          onClick={() => setInput(input + "%")}
-        >
+        <button className="operatorButton" onClick={() => console.log("%")}>
           %
         </button>
         <button
           className="operatorButton"
-          onClick={() => setInput(input + "/")}
+          onClick={() => {
+            handleSetOperands();
+            setOperator("/");
+            console.log("/");
+          }}
         >
           /
         </button>
-        <button onClick={() => addToInput("7")}>7</button>
-        <button onClick={() => setInput(input + "8")}>8</button>
-        <button onClick={() => setInput(input + "9")}>9</button>
+        <button onClick={() => addToOperand("7")}>7</button>
+        <button onClick={() => addToOperand("8")}>8</button>
+        <button onClick={() => addToOperand("9")}>9</button>
         <button
           className="operatorButton"
-          onClick={() => setInput(input + "*")}
+          onClick={() => {
+            handleSetOperands();
+            setOperator("*");
+            console.log("*");
+          }}
         >
           *
         </button>
-        <button onClick={() => setInput(input + "4")}>4</button>
-        <button onClick={() => setInput(input + "5")}>5</button>
-        <button onClick={() => setInput(input + "6")}>6</button>
+        <button onClick={() => addToOperand("4")}>4</button>
+        <button onClick={() => addToOperand("5")}>5</button>
+        <button onClick={() => addToOperand("6")}>6</button>
         <button
           className="operatorButton"
-          onClick={() => setInput(input + "-")}
+          onClick={() => {
+            handleSetOperands();
+            setOperator("-");
+            console.log("-");
+          }}
         >
           -
         </button>
-        <button onClick={() => setInput(input + "1")}>1</button>
-        <button onClick={() => setInput(input + "2")}>2</button>
-        <button onClick={() => setInput(input + "3")}>3</button>
+        <button onClick={() => addToOperand("1")}>1</button>
+        <button onClick={() => addToOperand("2")}>2</button>
+        <button onClick={() => addToOperand("3")}>3</button>
         <button
           className="operatorButton"
-          onClick={() => setInput(input + "+")}
+          onClick={() => {
+            handleSetOperands();
+            setOperator("+");
+            console.log("+");
+          }}
         >
           +
         </button>
-        <button className="zeroButton" onClick={() => setInput(input + "0")}>
+        <button className="zeroButton" onClick={() => addToOperand("0")}>
           0
         </button>
-        <button onClick={() => setInput(input + ".")}>.</button>
-        <button className="equalsButton" onClick={() => setInput(input + "=")}>
+        <button onClick={() => addToOperand(".")}>.</button>
+        <button
+          className="equalsButton"
+          onClick={() => {
+            const result = math.evaluate(firstOperand + operator + operand);
+            setDisplay(math.format(result, { precision: 7 }));
+            setFirstOperand(result);
+            console.log(firstOperand + operator + operand + "=" + result);
+          }}
+        >
           =
         </button>
       </section>
